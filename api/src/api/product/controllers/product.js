5b05a2b87d9a351e05453bb6fd4fae8fc0604cae
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2024-03-02 05:59:36 -0600

    chore

--- api/src/api/product/controllers/product.js
+++ api/src/api/product/controllers/product.js
@@ -1,15 +1,26 @@
 'use strict'
 
+const { constulCustom } = require('../../../hooks/consults')
 const { createCoreController } = require('@strapi/strapi').factories
 // @ts-ignore
 module.exports = createCoreController('api::product.product', ({ strapi }) => ({
-  async find () {
+  async find (params) {
+    const table = 'api::product.product'
+    let attributes = ['code', 'stock', 'sale_price']; 
+    const populate = {product_info: { select: ['id', 'name'] }}
+    /*if (params.request.url.includes('select')) {
+      const selectParam = new URLSearchParams(params.request.url.split('?')[1]).get('select');
+      if (selectParam) {
+        selectedAttributes = selectParam.split(',');
+      }
+    }
+    console.log('Atributos seleccionados:', selectedAttributes);
     const entry = await strapi.db.query('api::product.product').findMany({
-      select: ['code', 'stock', 'sale_price'],
+      select: selectedAttributes,
       populate: { product_info: { select: ['id', 'name', 'thumbnail'] } }
-    })
+    })*/
 
-    return entry
+    return constulCustom(params,attributes,table,populate)
   },
   async findOne () {
     const entry = await strapi.db.query('api::product.product').findOne({
--- client/src/hooks/useFetchProduct.js
+++ client/src/hooks/useFetchProduct.js
@@ -1,13 +1,16 @@
 'use client'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { NextResponse } from 'next/server'
 
 export function useFetchProduct () {
   const [products, setProducts] = useState([])
   useEffect(() => {
     const data = async () => {
-      const datas = await axios.get('http://localhost:1337/api/products').then((rest) => {
+      const datas = await axios.get('http://localhost:1337/api/products', {
+        params: {
+          select: 'code,stock'
+        }
+      }).then((rest) => {
         console.log('rest.data', rest.data)
         setProducts(rest.data)
       }).catch((error) => {
