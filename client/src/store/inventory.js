0f38749bb7462687b697d514f38ddfb7c3f262cf
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2024-03-06 20:21:59 -0600

    chore - extend consult

--- client/src/store/inventory.js
+++ client/src/store/inventory.js
@@ -2,6 +2,7 @@ import { create } from 'zustand'
 import axios from 'axios'
 
 import { PRODUCTS_ROOT } from '@/utils/config'
+import { LIST } from '@/utils/querys'
 
 export const useInventoryStore = create((set, get) => {
   return {
@@ -14,9 +15,11 @@ export const useInventoryStore = create((set, get) => {
     handleOpenProduct: (record) => set({ product: record, openProduct: true }),
     handleCancelProduct: () => set({ openProduct: false }),
     fetchProducts: async () => {
-      const res = await axios.get(PRODUCTS_ROOT)
+      const res = await axios.get(PRODUCTS_ROOT, LIST)
       const products = await res.data
-
+      // create = await axios.post(PRODUCTS_ROOT, CREATE)
+      // update = await axios.put(`${PRODUCTS_ROOT}/5`, UPDATE) el 5 es el id de el que se esta editando
+      // delete = await axios.delete(`${PRODUCTS_ROOT}/5`)
       set({ products, productCount: products.length }, false, 'FETCH_PRODUCTS')
     },
     action: 'add',
--- client/src/utils/querys.js
+++ client/src/utils/querys.js
@@ -1,6 +1,38 @@
 export const LIST = {
   params: {
-    select: 'code,stock',
-    populate: 'product_info:name-thumbnail'
+    select: 'code,stock,minimun_stock,purshes_price,sale_price,minimun_price,IVA,is_on_sale,is_deleted',
+    populate: 'product_info:name-thumbnail-description-safety_info.brand;name-color/category;name-color/area;name-color'
+  }
+}
+
+export const CREATE = {
+  data: {
+    code: 999999,
+    stock: 23,
+    minimun_stock: 23,
+    purshes_price: 23,
+    sale_price: 23,
+    minimun_price: 23,
+    IVA: 23,
+    is_on_sale: true,
+    is_deleted: false,
+    product_info: 1
+
+  }
+}
+
+export const UPDATE = {
+  data: {
+    code: 998899,
+    stock: 23,
+    minimun_stock: 23,
+    purshes_price: 343445,
+    sale_price: 23,
+    minimun_price: 23,
+    IVA: 23,
+    is_on_sale: true,
+    is_deleted: false,
+    product_info: 1
+
   }
 }
